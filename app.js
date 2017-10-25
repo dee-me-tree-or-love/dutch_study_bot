374aef4c3657576e9a6f08b2a2ec78d829695686
dmitrii <dmitry-orlov@hotmail.com>
2017-10-25 22:56:50 +0200

    changes

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+// const db = require('sqlite');
+
+const app = express();
+let server;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* App startup */
+let p = new Promise((resolve, reject) => { resolve() });
+p.then(() => db.open('./db/database.sqlite', { Promise }))
+    .then(() => db.migrate({ force: 'last' }))
+    .catch(err => console.error(err.stack))
+    .then(() => {
+        server = app.listen(process.env.PORT || 5000, () => {
+            console.log('Express server listening on port %d in %s mode',
+                server.address().port, app.settings.env);
+        });
+    });
+
+/* Index */
+app.get('/', function (req, res) {
+    res.send('Hello Stranger! What brought you here?')
+})
