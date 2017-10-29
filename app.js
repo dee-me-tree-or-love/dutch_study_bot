ae19a2e2879abf640405059f6b297c5e13e53c4c
dmitrii <dmitry-orlov@hotmail.com>
2017-10-29 13:49:23 +0100

    added the api submodule, starting to refactor the application

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 /* App startup */
 let p = new Promise((resolve, reject) => { resolve() });
-p.then(() => db.open('./db/database.sqlite', { Promise }))
+p.then(() => db.open('./db/database.sqlite', { Promise })) // open the database -> change to neo4j
     .then(() => db.migrate({ force: 'last' }))
     .catch(err => console.error(err.stack))
     .then(() => {
@@ -26,3 +26,5 @@ p.then(() => db.open('./db/database.sqlite', { Promise }))
 app.get('/', function (req, res) {
     res.send('Hello Stranger! What brought you here?')
 })
+
+module.exports = app;
