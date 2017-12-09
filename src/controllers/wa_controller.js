785b0199983d6275f9e48bac7d35f574facfc8f6
dmitrii <dmitry-orlov@hotmail.com>
2017-12-09 13:46:45 +0100

    prepared add word method

--- src/controllers/wa_controller.js
+++ src/controllers/wa_controller.js
@@ -1,13 +1,13 @@
 // TODO: maybe should be moved to the different source root?
-  // It is a special case of a controler, so...
+// It is a special case of a controler, so...
 // TODO: maybe make it an sdk for the wordeu-api for JS, so then, 
-  // only one implementation would be enouh, and all the extra stuff can
-  // either be an extension of the class or added with an open issue in GH
+// only one implementation would be enouh, and all the extra stuff can
+// either be an extension of the class or added with an open issue in GH
 const request = require('request');
 // in order to return the request promises, this library is used
-  // to avoid extra dependencies and keep it to the native promises, this one is 
-  // used instead of 'request-promise', which uses BlueBird promises
-  // for usage api refer to: https://www.npmjs.com/package/request-promise
+// to avoid extra dependencies and keep it to the native promises, this one is 
+// used instead of 'request-promise', which uses BlueBird promises
+// for usage api refer to: https://www.npmjs.com/package/request-promise
 const requestPromise = require('request-promise-native');
 
 module.exports = class WordeuApiController {
@@ -38,5 +38,47 @@ module.exports = class WordeuApiController {
     // could also go with just making a new Promise object 
     // and inside using regular request library, but huh, this is neat
     return requestPromise(options);
+  }
+
+  /**
+   * Creates a new word entry in the wordeu database and connects it to the user
+   * FIXME: this approach kinda sucks because this could be done in one API call
+   * @param {*} term 
+   * @param {*} pageId 
+   * @returns {Promise} a promise chain as explained in definition 
+   */
+  addLearningWord(term, pageId) {
+    const create = {
+      method: 'POST',
+      uri: `${this.HOST}words/new`,
+      headers: {
+        'User-Agent': 'WordeuApiController'
+      },
+      body: {
+        title: term,
+      },
+      json: true
+    }
+
+    // localhost:8008/users/pid/121/add/word
+
+    const p = requestPromise(create)
+      .then((res) => {
+        // FIXME: this is rather insecure
+        const link = {
+          method: 'POST',
+          uri: `${this.HOST}users/pid/${pageId}/add/word`,
+          headers: {
+            'User-Agent': 'WordeuApiController'
+          },
+          body: {
+            targetTitle: res.title,
+            relationType: 'learns'
+          },
+          json: true
+        }
+        return requestPromise(link);
+      })
+    return p;
   }
 }
